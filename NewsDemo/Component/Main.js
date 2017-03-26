e580551a73597fb79b7c1fa0d9752fd89242d8b7
bocai <512517786@qq.com>
2017-03-26 22:50:44 +0800

    NewsDemo综合示例

---
+++ NewsDemo/Component/Main.js
@@ -0,0 +1,34 @@
+/**
+ * Created by guoqingping on 2017/3/26.
+ */
+import React, { Component } from 'react';
+import {
+    AppRegistry,
+    StyleSheet,
+    Text,
+    View
+} from 'react-native';
+
+var Main = React.createClass({
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text>主页</Text>
+            </View>
+        );
+    }
+})
+
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#F5FCFF',
+    },
+  
+});
+
+
+module.exports = Main;
---
+++ NewsDemo/index.ios.js
@@ -0,0 +1,51 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+
+
+
+//引入外部组件
+var Main = require("./Component/Main");
+
+var NewsDemo = React.createClass({
+  render() {
+    return (
+        <View style={styles.container}>
+            <Main></Main>
+        </View>
+    );
+  }
+})
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#333333',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('NewsDemo', () => NewsDemo);
